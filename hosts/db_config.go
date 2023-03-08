a779271fe09923e82f4b9d57adafb11e32da510d
Mustafa3011 <officewalamustafa@gmail.com>
2023-03-08 09:48:14 +0530

    containerize

--- hosts/db_config.go
+++ hosts/db_config.go
@@ -7,7 +7,7 @@ import (
 )
 
 func db_setup() *sql.DB {
-	db, err := sql.Open("postgres", "postgres://locallookup:1234@localhost:5432/chatit_v2")
+	db, err := sql.Open("postgres", "postgres://locallookup:1234@localhost:5432/chatit")
 	if err != nil {
 		println(err.Error())
 	} else if db.Ping() != nil {
