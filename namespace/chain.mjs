a12b763fbf7a4b9412000a28669d85e8448f85b3
Mustafa3011 <officewalamustafa@gmail.com>
2023-03-15 14:19:11 +0530

    contract addr resolve

--- namespace/chain.mjs
+++ namespace/chain.mjs
@@ -1,9 +1,10 @@
 import { ethers } from "ethers";
 import ganache from "ganache";
+import fs from 'fs';
 import Database from './build/contracts/Database.json' assert {type: "json"};
 
 const PRIVATE_KEY = "0x5b3208286264f409e1873e3709d3138acf47f6cc733e74a6b47a040b50472fd8";
-const CONTRACT_ADDRESS = "0x50e7d57bb32fe6b98783591d113722988a7ad5c8";
+const CONTRACT_ADDRESS = fs.readFileSync("CONTRACT_ADDRESS").toString();
 
 const provider = new ethers.providers.Web3Provider(ganache.provider({
     database: {
